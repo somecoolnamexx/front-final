b89d0bdd3c3e93b4a9825c4d83aa45c14cced7f1
datotoda <datotoda1342@gmail.com>
2023-06-23 01:05:18 +0400

    Improved responsiveness of features in landing page

--- public/src/js/common.js
+++ public/src/js/common.js
@@ -307,7 +307,7 @@ footer.innerHTML = `
 </ul>
 <div class="d-flex flex-wrap justify-content-between align-items-center px-sm-5">
     <div class="col-md-4 d-flex align-items-center">
-        <span class="mb-3 mb-md-0 text-muted">© 2023 Davit Chinchaladze</span>
+        <span class="text-muted">© 2023 Davit Chinchaladze</span>
     </div>
 
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
